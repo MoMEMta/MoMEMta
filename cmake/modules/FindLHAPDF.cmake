722684bdfa9d2306976b1a3eb77570aade3b0a6a
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-01-14 17:59:39 +0100

    Require at least LHAPDF 6.0

--- cmake/modules/FindLHAPDF.cmake
+++ cmake/modules/FindLHAPDF.cmake
@@ -3,20 +3,27 @@
 #     LHAPDF_FOUND           - System has LHAPDF installed
 #     LHAPDF_INCLUDE_DIRS    - LHAPDF include directories
 #     LHAPDF_LIBRARIES       - LHAPDF library
+#     LHAPDF_VERSION         - LHAPDF version
 
 find_program(LHAPDF_CONFIG_PROGRAM lhapdf-config)
 
 if (LHAPDF_CONFIG_PROGRAM)
+    exec_program( ${LHAPDF_CONFIG_PROGRAM} ARGS "--version" OUTPUT_VARIABLE
+        LHAPDF_VERSION )
     exec_program( ${LHAPDF_CONFIG_PROGRAM} ARGS "--incdir" OUTPUT_VARIABLE LHAPDF_INCLUDE_DIRS )
     exec_program( ${LHAPDF_CONFIG_PROGRAM} ARGS "--libdir" OUTPUT_VARIABLE LHAPDF_LIBRARY_DIR )
 
     find_library( LHAPDF_LIBRARY LHAPDF HINTS ${LHAPDF_LIBRARY_DIR} )
     
     set( LHAPDF_LIBRARIES ${LHAPDF_LIBRARY} )
-
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LHAPDF DEFAULT_MSG LHAPDF_LIBRARIES LHAPDF_INCLUDE_DIRS)    
+find_package_handle_standard_args(
+    LHAPDF
+    FOUND_VAR LHAPDF_FOUND
+    REQUIRED_VARS LHAPDF_LIBRARIES LHAPDF_INCLUDE_DIRS
+    VERSION_VAR LHAPDF_VERSION
+    )
 
-mark_as_advanced(LHAPDF_INCLUDE_DIRS LHAPDF_LIBRARY_DIR LHAPDF_LIBRARY )
+mark_as_advanced(LHAPDF_VERSION LHAPDF_INCLUDE_DIRS LHAPDF_LIBRARY_DIR LHAPDF_LIBRARY )
