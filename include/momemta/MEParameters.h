d0d31629074b1808f78d5211145cd939c797d4a9
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-03-24 17:16:01 +0100

    Add public interface for MatrixElement and parameters

---
+++ include/momemta/MEParameters.h
@@ -0,0 +1,49 @@
+/*
+ *  MoMEMta: a modular implementation of the Matrix Element Method
+ *  Copyright (C) 2016  Universite catholique de Louvain (UCL), Belgium
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifndef MOMEMTA_MEPARAMETERS_H
+#define MOMEMTA_MEPARAMETERS_H
+
+#include <unordered_map>
+
+// FIXME: Decide if we use a namespace or not?
+namespace momemta {
+
+    class MEParameters {
+        public:
+            MEParameters() = default;
+            virtual ~MEParameters() {};
+
+            /**
+             * Cache parameters. This is implementation dependent
+             */
+            virtual void cacheParameters() = 0;
+            virtual void cacheCouplings() = 0;
+
+            virtual void updateParameters() = 0; 
+            virtual void updateCouplings() = 0;
+
+        protected:
+            std::unordered_map<std::string, double> m_card_parameters;
+    };
+
+};
+
+#endif
---
+++ include/momemta/MatrixElement.h
@@ -0,0 +1,57 @@
+/*
+ *  MoMEMta: a modular implementation of the Matrix Element Method
+ *  Copyright (C) 2016  Universite catholique de Louvain (UCL), Belgium
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifndef MOMEMTA_MATRIXELEMENT_H
+#define MOMEMTA_MATRIXELEMENT_H
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <momemta/MEParameters.h>
+
+class ConfigurationSet;
+
+// FIXME Namespace or not?
+namespace momemta {
+    
+    class MatrixElement {
+        public:
+            using Result = std::map<std::pair<int, int>, double>;
+
+            MatrixElement() = default;
+            virtual ~MatrixElement() {};
+    
+            virtual Result compute(
+                    const std::vector<std::vector<double>>& initialMomenta,
+                    const std::vector<std::pair<int, std::vector<double>>>& finalState
+                    ) = 0;
+
+            virtual std::shared_ptr<MEParameters> getParameters() = 0;
+            
+            // FIXME: Needed?
+            virtual std::string name() const = 0;
+            virtual const std::vector<double>& getMasses() const = 0;
+    };
+
+}
+
+#endif
