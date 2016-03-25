1a42bb2194eb276e5a09bb4574c8cf900ad078cd
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-03-25 11:11:22 +0100

    Use colors when logging in terminal

--- core/include/logging.h
+++ core/include/logging.h
@@ -16,20 +16,84 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #pragma once
 
 #include <spdlog/spdlog.h>
 
 namespace logging {
-    std::shared_ptr<spdlog::logger>& get();
+
+inline std::string color(uint16_t color) {
+    return "\033[" + std::to_string(color) + "m";
 }
 
-#define LOG(severity) \
-    ::logging::get()->severity()
+class colored_formatter: public spdlog::formatter
+{
+    void format(spdlog::details::log_msg& msg) override
+    {
+#ifndef SPDLOG_NO_DATETIME
+        auto tm_time = spdlog::details::os::localtime(spdlog::log_clock::to_time_t(msg.time));
+        auto duration = msg.time.time_since_epoch();
+        auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
 
-#define LOGGER \
-    ::logging::get()
+        msg.formatted << '[' << static_cast<unsigned int>(tm_time.tm_year + 1900) << '-'
+                      << fmt::pad(static_cast<unsigned int>(tm_time.tm_mon + 1), 2, '0') << '-'
+                      << fmt::pad(static_cast<unsigned int>(tm_time.tm_mday), 2, '0') << ' '
+                      << fmt::pad(static_cast<unsigned int>(tm_time.tm_hour), 2, '0') << ':'
+                      << fmt::pad(static_cast<unsigned int>(tm_time.tm_min), 2, '0') << ':'
+                      << fmt::pad(static_cast<unsigned int>(tm_time.tm_sec), 2, '0') << '.'
+                      << fmt::pad(static_cast<unsigned int>(millis), 3, '0') << "] ";
 
-#define TRACE(x...) \
-    SPDLOG_TRACE(::logging::get(), x)
+//no datetime needed
+#else
+        (void)tm_time;
+#endif
+
+#ifndef SPDLOG_NO_NAME
+        msg.formatted << '[' << msg.logger_name << "] ";
+#endif
+
+        msg.formatted << '[';
+        switch (msg.level) {
+            case spdlog::level::trace:
+            case spdlog::level::debug:
+                msg.formatted << color(90);
+                break;
+            case spdlog::level::warn:
+                msg.formatted << color(33);
+                break;
+            case spdlog::level::err:
+            case spdlog::level::critical:
+            case spdlog::level::alert:
+            case spdlog::level::emerg:
+                msg.formatted << color(31);
+                break;
+            default:
+                break;
+        }
+        msg.formatted << spdlog::level::to_str(msg.level) << color(0) << "] ";
+        msg.formatted << fmt::StringRef(msg.raw.data(), msg.raw.size());
+        msg.formatted << spdlog::details::os::eol();
+    }
+};
+
+template <class Mutex> class colored_stdout_sink {
+  public:
+
+    static std::shared_ptr<spdlog::sinks::stdout_sink<Mutex>> instance() {
+        static std::shared_ptr<spdlog::sinks::stdout_sink<Mutex>> instance = std::make_shared<spdlog::sinks::stdout_sink<Mutex>>();
+        instance->set_formatter(std::make_shared<colored_formatter>());
+        return instance;
+    }
+};
+
+typedef colored_stdout_sink<spdlog::details::null_mutex> colored_stdout_sink_st;
+
+std::shared_ptr<spdlog::logger>& get();
+
+}
+
+#define LOG(severity) ::logging::get()->severity()
+
+#define LOGGER ::logging::get()
+
+#define TRACE(x...) SPDLOG_TRACE(::logging::get(), x)
