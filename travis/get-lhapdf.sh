ede21d72ebf4f9a30b41a8ee5053316c42d1cb24
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-04-17 22:29:54 +0200

    [Docs] Update doxygen to v1.8.11

--- travis/get-lhapdf.sh
+++ travis/get-lhapdf.sh
@@ -14,8 +14,6 @@ cd ${BUNDLE}
 # Fix 'lhapdf-config' which hardcode path
 sed -i "s#/vagrant/builds/LHAPDF-6.1.6_Python-2.7_gcc-4.9.3_Ubuntu_14.04_x86_64#$PWD#" bin/lhapdf-config
 
-cat bin/lhapdf-config
-
 export PATH="$PWD/bin:$PATH"
 export LD_LIBRARY_PATH="$PWD/lib:$LB_LIBRARY_PATH"
 
