b21cb79b63a961b5ca616c3de3bf6c594911d005
Sébastien Wertz <swertz@users.noreply.github.com>
2016-05-17 16:57:58 +0200

    Update README (#52)

--- examples/tt_fullyleptonic_NWA.cc
+++ examples/tt_fullyleptonic_NWA.cc
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
     UNUSED(argc);
     UNUSED(argv);
 
-    logging::set_level(boost::log::trivial::trace);
+    logging::set_level(boost::log::trivial::debug);
 
     ConfigurationReader configuration("../examples/tt_fullyleptonic_NWA.lua");
     MoMEMta weight(configuration.freeze());
