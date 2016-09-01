43bd06b9cbf8512d85758827b509302f0fbc3fe3
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-09-01 16:42:19 +0200

    Added new Constant module
    This module creates a simple constant that can be
    referenced by other modules by an InputTag

---
+++ modules/Constant.cc
@@ -0,0 +1,50 @@
+/*
+ *  MoMEMta: a modular implementation of the Matrix Element Method
+ *  Copyright (C) 2016  Universite catholique de Louvain (UCL), Belgium
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <momemta/Module.h>
+
+#include <momemta/ParameterSet.h>
+
+/**
+ * \brief A module declaring a constant
+ */
+template<typename T>
+class Constant: public Module {
+    public:
+
+    Constant(PoolPtr pool, const ParameterSet& parameters): Module(pool, parameters.getModuleName()) {
+            value = parameters.get<T>("value");
+        };
+
+        virtual void beginIntegration() const {
+            *constant = value;
+        };
+
+        virtual bool leafModule() const override {
+            return true;
+        }
+
+    private:
+        T value;
+
+        // Outputs
+        std::shared_ptr<T> constant = produce<T>("value");
+};
+
+REGISTER_MODULE_NAME("IntConstant", Constant<int64_t>);
+REGISTER_MODULE_NAME("DoubleConstant", Constant<double>);
