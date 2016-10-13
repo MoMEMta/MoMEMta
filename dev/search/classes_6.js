e2c9c19f5ead7449d94f1881662f43468d367bf4
Documentation Builder <momemta@cp3.be>
2016-10-13 10:05:37 +0000

    Automated documentation build for changeset 4fc35f54fbc819627af5808c9cfd6225172a5880.

--- dev/search/classes_6.js
+++ dev/search/classes_6.js
@@ -2,5 +2,8 @@ var searchData=
 [
   ['gaussiantransferfunctiononenergy',['GaussianTransferFunctionOnEnergy',['../classGaussianTransferFunctionOnEnergy.html',1,'']]],
   ['gaussiantransferfunctiononenergybase',['GaussianTransferFunctionOnEnergyBase',['../classGaussianTransferFunctionOnEnergyBase.html',1,'']]],
-  ['gaussiantransferfunctiononenergyevaluator',['GaussianTransferFunctionOnEnergyEvaluator',['../classGaussianTransferFunctionOnEnergyEvaluator.html',1,'']]]
+  ['gaussiantransferfunctiononenergyevaluator',['GaussianTransferFunctionOnEnergyEvaluator',['../classGaussianTransferFunctionOnEnergyEvaluator.html',1,'']]],
+  ['gaussiantransferfunctiononpt',['GaussianTransferFunctionOnPt',['../classGaussianTransferFunctionOnPt.html',1,'']]],
+  ['gaussiantransferfunctiononptbase',['GaussianTransferFunctionOnPtBase',['../classGaussianTransferFunctionOnPtBase.html',1,'']]],
+  ['gaussiantransferfunctiononptevaluator',['GaussianTransferFunctionOnPtEvaluator',['../classGaussianTransferFunctionOnPtEvaluator.html',1,'']]]
 ];
