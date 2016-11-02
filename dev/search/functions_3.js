d53b37c5ba93b3787fd6139feb92a20f45ae4741
Documentation Builder <momemta@cp3.be>
2016-11-02 10:33:36 +0000

    Automated documentation build for changeset b014c6d4ab7f5ea46bbf331f7b458116fb071268.

--- dev/search/functions_3.js
+++ dev/search/functions_3.js
@@ -3,7 +3,7 @@ var searchData=
   ['clone',['clone',['../classlua_1_1LazyTable.html#ae5fbdda7f5860cb8648797c0cffff008',1,'lua::LazyTable::clone()'],['../classParameterSet.html#a90562b2a6136b64d5918db6b92d22094',1,'ParameterSet::clone()']]],
   ['computeweights',['computeWeights',['../classMoMEMta.html#afc501dc0bbc0512f4ec528dbd74729e6',1,'MoMEMta']]],
   ['configuration',['Configuration',['../classConfiguration.html#a7a77596f11e5986dfef897fe7554a0b5',1,'Configuration::Configuration(const Configuration &amp;)'],['../classConfiguration.html#a36794d3e40bbf41ac7a71c84c6afa026',1,'Configuration::Configuration(const Configuration &amp;&amp;)']]],
-  ['configure',['configure',['../classModule.html#a1e559c32bbb1442a3100bb6d9c90bca3',1,'Module']]],
+  ['configure',['configure',['../classModule.html#a1e559c32bbb1442a3100bb6d9c90bca3',1,'Module::configure()'],['../classLooper.html#a03f32cd9643774bb9e4a6d505e160f06',1,'Looper::configure()']]],
   ['create',['create',['../classlua_1_1LazyTable.html#a1c54fa36db8ff5dba94989182239f9ed',1,'lua::LazyTable::create()'],['../classParameterSet.html#aed64663b10836556a3727eea33cb32d3',1,'ParameterSet::create()']]],
   ['current_5fmodule',['current_module',['../classPool.html#af3a1e17a97dc3618de1a0955c7f160a9',1,'Pool::current_module(const Configuration::Module &amp;module) final'],['../classPool.html#a923e7e3eea70827607639137e7b7c97c',1,'Pool::current_module(const std::string &amp;name) final']]]
 ];
--- dev/search/functions_d.js
+++ dev/search/functions_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['rawget',['rawGet',['../classParameterSet.html#ae7c56f94765e4f1aef16042e9131ca42',1,'ParameterSet']]],
-  ['register_5fmodules',['register_modules',['../namespacelua.html#af97c98bd682a6a4a083fd71d37fcf22b',1,'lua']]],
-  ['resolved',['resolved',['../classPath.html#ae165f9f0f519178d857bff755db6486b',1,'Path']]]
+  ['register_5fmodules',['register_modules',['../namespacelua.html#af97c98bd682a6a4a083fd71d37fcf22b',1,'lua']]]
 ];
