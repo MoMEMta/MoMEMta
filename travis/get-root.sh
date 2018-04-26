41271fa040af36abe33344849f015ca77eab89ca
Christophe Delaere <christophe.delaere@cern.ch>
2018-04-26 15:27:45 +0200

    new path to travis build bundles (#173)

--- travis/get-root.sh
+++ travis/get-root.sh
@@ -3,7 +3,7 @@
 mkdir -p external/root
 cd external/root
 
-wget http://sbrochet.web.cern.ch/sbrochet/public/ROOT-${ROOT_VERSION}_Python-2.7_gcc-4.9.3_Ubuntu_14.04_x86_64.tar.xz
+wget http://cp3.irmp.ucl.ac.be/~delaere/MoMEMta/ROOT-${ROOT_VERSION}_Python-2.7_gcc-4.9.3_Ubuntu_14.04_x86_64.tar.xz
 
 tar xf ROOT-${ROOT_VERSION}_Python-2.7_gcc-4.9.3_Ubuntu_14.04_x86_64.tar.xz
 cd ROOT-${ROOT_VERSION}_Python-2.7_gcc-4.9.3_Ubuntu_14.04_x86_64
