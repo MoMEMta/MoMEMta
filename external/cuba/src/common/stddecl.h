8e71cb372ab4dc5a144abd0ad8e6640185713c09
Sebastien Wertz <sebastien.wertz@cern.ch>
2021-06-08 11:13:40 +0200

    some cuba patches were missing

--- external/cuba/src/common/stddecl.h
+++ external/cuba/src/common/stddecl.h
@@ -121,7 +121,7 @@ enum { uninitialized = 0x61627563 };
 #define POW2(n) ldexp(1., -n)
 #endif
 
-#define NOTZERO POW2(250)
+#define NOTZERO POW2(1000)
 
 #define ABORT -999
 
@@ -215,6 +215,7 @@ enum { uninitialized = 0x61627563 };
   who##Alloc(t); \
   if( t->shmid != -1 ) { \
     t->frame = shmat(t->shmid, NULL, 0); \
+    shmctl(t->shmid, IPC_RMID, NULL); \
     if( t->frame == (void *)-1 ) Abort("shmat"); \
   }
 
